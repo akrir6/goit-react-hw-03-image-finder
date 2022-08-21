b38e0001704bfea4ad617c79c1e89872ac8043ae
Roman Radakhevych <akrir@ukr.net>
2022-08-21 16:59:35 +0300

    copy hw template

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+import { Container } from "./App.styled";
+export class App extends Component {
+   state = {
+   
+  }
+
+  componentDidMount() {
+   
+  }
+
+  componentDidUpdate(_, prevState) {
+    
+  }
+  
+  
+
+  render() {
+    
+    return (
+      <Container>
+        <h1>踊りましょう</h1>
+      </Container>
+    )
+  }
+}
+
